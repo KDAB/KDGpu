5caebfc9f572945e0acce6ea48cf3d71a31f5cc3
Sean Harmer <sean.harmer@kdab.com>
2022-11-23 11:19:34 +0000

    Add members to vulkan_buffer

--- src/toy_renderer/vulkan/vulkan_buffer.cpp
+++ src/toy_renderer/vulkan/vulkan_buffer.cpp
@@ -2,4 +2,13 @@
 
 namespace ToyRenderer {
 
+VulkanBuffer::VulkanBuffer(VkBuffer _buffer,
+                           VulkanResourceManager *_vulkanResourceManager,
+                           const Handle<Device_t> &_deviceHandle)
+    : buffer(_buffer)
+    , vulkanResourceManager(_vulkanResourceManager)
+    , device(_device)
+{
 }
+
+} // namespace ToyRenderer
--- src/toy_renderer/vulkan/vulkan_buffer.h
+++ src/toy_renderer/vulkan/vulkan_buffer.h
@@ -2,12 +2,24 @@
 
 #include <toy_renderer/api/api_buffer.h>
 
+#include <toy_renderer/handle.h>
+
 #include <vulkan/vulkan.h>
 
 namespace ToyRenderer {
 
+class VulkanResourceManager;
+
+struct Device_t;
 struct VulkanBuffer : public ApiBuffer {
-    VkBuffer buffer;
+    VulkanBuffer(VkBuffer _buffer,
+                 VulkanResourceManager *_vulkanResourceManager,
+                 const Handle<Device_t> &_deviceHandle);
+
+    VkBuffer buffer{ VK_NULL_HANDLE };
+
+    VulkanResourceManager *vulkanResourceManager;
+    Handle<Device_t> deviceHandle;
 };
 
 } // namespace ToyRenderer
