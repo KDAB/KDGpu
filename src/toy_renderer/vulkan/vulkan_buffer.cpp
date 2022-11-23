3d691d08b7053da22db0837aa6abf8b36bc36f3a
Sean Harmer <sean.harmer@kdab.com>
2022-11-23 11:52:17 +0000

    Link to vma and add allocation to vulkan_buffer

--- src/toy_renderer/vulkan/vulkan_buffer.cpp
+++ src/toy_renderer/vulkan/vulkan_buffer.cpp
@@ -3,9 +3,11 @@
 namespace ToyRenderer {
 
 VulkanBuffer::VulkanBuffer(VkBuffer _buffer,
+                           VmaAllocation _allocation,
                            VulkanResourceManager *_vulkanResourceManager,
                            const Handle<Device_t> &_deviceHandle)
     : buffer(_buffer)
+    , allocation(_allocation)
     , vulkanResourceManager(_vulkanResourceManager)
     , device(_device)
 {
--- src/toy_renderer/vulkan/vulkan_buffer.h
+++ src/toy_renderer/vulkan/vulkan_buffer.h
@@ -4,6 +4,7 @@
 
 #include <toy_renderer/handle.h>
 
+#include <vk_mem_alloc.h>
 #include <vulkan/vulkan.h>
 
 namespace ToyRenderer {
@@ -13,10 +14,13 @@ class VulkanResourceManager;
 struct Device_t;
 struct VulkanBuffer : public ApiBuffer {
     VulkanBuffer(VkBuffer _buffer,
+                 VmaAllocation _allocation,
                  VulkanResourceManager *_vulkanResourceManager,
                  const Handle<Device_t> &_deviceHandle);
 
     VkBuffer buffer{ VK_NULL_HANDLE };
+    VmaAllocation allocation{ VK_NULL_HANDLE };
+    void *mapped{ nullptr };
 
     VulkanResourceManager *vulkanResourceManager;
     Handle<Device_t> deviceHandle;
