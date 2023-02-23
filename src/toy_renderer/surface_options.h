b55cffde0526249b2ab337864067c9cf3bf2d279
Sean Harmer <sean.harmer@kdab.com>
2023-02-23 09:31:45 +0000

    Use toy renderer platform defines in surface_options.h

--- src/toy_renderer/surface_options.h
+++ src/toy_renderer/surface_options.h
@@ -1,31 +1,29 @@
 #pragma once
 
-#include <KDFoundation/config.h>
-
-#if defined(KD_PLATFORM_WIN32)
+#if defined(TOY_RENDERER_PLATFORM_WIN32)
 #define NOMINMAX
 #include <windows.h>
 #endif
 
-#if defined(KD_PLATFORM_LINUX)
+#if defined(TOY_RENDERER_PLATFORM_LINUX)
 #include <xcb/xproto.h>
 #endif
 
-#if defined(KD_PLATFORM_MACOS)
+#if defined(TOY_RENDERER_PLATFORM_MACOS)
 #endif
 
 namespace ToyRenderer {
 
 struct SurfaceOptions {
-#if defined(KD_PLATFORM_WIN32)
+#if defined(TOY_RENDERER_PLATFORM_WIN32)
     HWND hWnd;
 #endif
-#if defined(KD_PLATFORM_LINUX)
+#if defined(TOY_RENDERER_PLATFORM_LINUX)
     // TODO: Add Wayland support
     xcb_connection_t *connection;
     xcb_window_t window;
 #endif
-#if defined(KD_PLATFORM_MACOS)
+#if defined(TOY_RENDERER_PLATFORM_MACOS)
     CAMetalLayer *layer;
 #endif
 };
