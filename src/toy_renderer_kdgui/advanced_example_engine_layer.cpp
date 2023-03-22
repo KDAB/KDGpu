50684a63b590e8f72efc84f049c7b7198550baad
Sean Harmer <sean.harmer@kdab.com>
2023-03-22 11:32:22 +0000

    Refactor the example layers to share code
    As a drive-by put them into the lib namespace.
    
    Change-Id: I0f08b1df0c4567aa9a98f34a9b6dd7268f646195
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/123575
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/toy_renderer_kdgui/advanced_example_engine_layer.cpp
+++ src/toy_renderer_kdgui/advanced_example_engine_layer.cpp
@@ -1,14 +1,11 @@
 #include "advanced_example_engine_layer.h"
 
-#include <KDGui/gui_application.h>
 #include <toy_renderer_kdgui/engine.h>
 
-#include <toy_renderer/swapchain_options.h>
-#include <toy_renderer/texture_options.h>
+namespace ToyRendererKDGui {
 
 AdvancedExampleEngineLayer::AdvancedExampleEngineLayer()
-    : EngineLayer()
-    , m_api(std::make_unique<VulkanGraphicsApi>())
+    : ExampleEngineLayer()
 {
 }
 
@@ -18,91 +15,22 @@ AdvancedExampleEngineLayer::~AdvancedExampleEngineLayer()
 
 void AdvancedExampleEngineLayer::onAttached()
 {
-    m_window = std::make_unique<View>();
-
-    // Request an instance of the api with whatever layers and extensions we wish to request.
-    // TODO: Pass these options in to the layer
-    InstanceOptions instanceOptions = {
-        .applicationName = KDGui::GuiApplication::instance()->objectName(),
-        .applicationVersion = SERENITY_MAKE_API_VERSION(0, 1, 0, 0)
-    };
-    m_instance = m_api->createInstance(instanceOptions);
-
-    // Create a drawable surface
-    m_surface = m_window->createSurface(m_instance);
-
-    // Create a device and a queue to use
-    auto defaultDevice = m_instance.createDefaultDevice(m_surface);
-    m_device = std::move(defaultDevice.device);
-    m_queue = m_device.queues()[0];
-
-    recreateSwapChain();
+    ExampleEngineLayer::onAttached();
 
     // Create the present complete and render complete semaphores
-    for (uint32_t i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i) {
-        m_presentCompleteSemaphores[i] = m_device.createGpuSemaphore();
-        m_renderCompleteSemaphores[i] = m_device.createGpuSemaphore();
+    for (uint32_t i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i)
         m_frameFences[i] = m_device.createFence();
-    }
 
     initializeScene();
-}
-
-void AdvancedExampleEngineLayer::recreateSwapChain()
-{
-    // Create a swapchain of images that we will render to.
-    SwapchainOptions swapchainOptions = {
-        .surface = m_surface,
-        .format = m_swapchainFormat,
-        .imageExtent = { .width = m_window->width(), .height = m_window->height() },
-        .oldSwapchain = m_swapchain,
-    };
-
-    // Create swapchain and destroy previous one implicitly
-    m_swapchain = m_device.createSwapchain(swapchainOptions);
-
-    const auto &swapchainTextures = m_swapchain.textures();
-    const auto swapchainTextureCount = swapchainTextures.size();
-
-    m_swapchainViews.clear();
-    m_swapchainViews.reserve(swapchainTextureCount);
-    for (uint32_t i = 0; i < swapchainTextureCount; ++i) {
-        auto view = swapchainTextures[i].createView({ .format = swapchainOptions.format });
-        m_swapchainViews.push_back(std::move(view));
-    }
-
-    // Create a depth texture to use for depth-correct rendering
-    TextureOptions depthTextureOptions = {
-        .type = TextureType::TextureType2D,
-        .format = Format::D24_UNORM_S8_UINT,
-        .extent = { m_window->width(), m_window->height(), 1 },
-        .mipLevels = 1,
-        .usage = TextureUsageFlags(TextureUsageFlagBits::DepthStencilAttachmentBit),
-        .memoryUsage = MemoryUsage::GpuOnly
-    };
-    m_depthTexture = m_device.createTexture(depthTextureOptions);
-    m_depthTextureView = m_depthTexture.createView();
 }
 
 void AdvancedExampleEngineLayer::onDetached()
 {
     // Wait until all commands have completed execution
     m_device.waitUntilIdle();
-
-    cleanupScene();
-
-    m_presentCompleteSemaphores = {};
-    m_renderCompleteSemaphores = {};
     m_frameFences = {};
-    m_depthTextureView = {};
-    m_depthTexture = {};
-    m_swapchainViews.clear();
-    m_swapchain = {};
-    m_queue = {};
-    m_device = {};
-    m_surface = {};
-    m_instance = {};
-    m_window = {};
+
+    ExampleEngineLayer::onDetached();
 }
 
 void AdvancedExampleEngineLayer::update()
@@ -157,3 +85,5 @@ void AdvancedExampleEngineLayer::update()
     // Waiting for Fences at the beginning of this functions prevents
     // us preparing more frames than MAX_FRAMES_IN_FLIGHT
 }
+
+} // namespace ToyRendererKDGui
