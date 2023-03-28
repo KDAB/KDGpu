48d0815828b9359dd85754a5e1f4a3dad4f53023
Paul Lemire <paul.lemire@kdab.com>
2023-03-28 16:21:52 +0200

    Dependencies: only fetch if target doesn't exist
    Change-Id: Iea18715444c0d7a394f8312ddc102e0d07beafbb
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/123985
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- cmake/dependencies/doctest.cmake
+++ cmake/dependencies/doctest.cmake
@@ -1,20 +1,23 @@
-FetchContent_Declare(
-    doctest
-    GIT_REPOSITORY https://github.com/doctest/doctest.git
-    GIT_TAG v2.4.8
+if(NOT TARGET doctest::doctest)
+    FetchContent_Declare(
+        doctest
+        GIT_REPOSITORY https://github.com/doctest/doctest.git
+        GIT_TAG v2.4.8
     )
 
-FetchContent_GetProperties(doctest)
-if(NOT doctest_POPULATED)
-    FetchContent_Populate(doctest)
+    FetchContent_GetProperties(doctest)
 
-    add_library(doctest INTERFACE)
+    if(NOT doctest_POPULATED)
+        FetchContent_Populate(doctest)
+
+        add_library(doctest INTERFACE)
+    endif()
+
+    target_include_directories(doctest INTERFACE
+        $<BUILD_INTERFACE:${doctest_SOURCE_DIR}/doctest>
+        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/doctest>
+    )
+    add_library(doctest::doctest ALIAS doctest)
+
+    install(DIRECTORY ${doctest_SOURCE_DIR}/doctest/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/doctest)
 endif()
-
-target_include_directories(doctest INTERFACE
-$<BUILD_INTERFACE:${doctest_SOURCE_DIR}/doctest>
-$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/Serenity/doctest>
-)
-add_library(Serenity::doctest ALIAS doctest)
-
-install(DIRECTORY ${doctest_SOURCE_DIR}/doctest/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/Serenity/doctest)
--- cmake/dependencies/trompeloeil.cmake
+++ cmake/dependencies/trompeloeil.cmake
@@ -1,15 +1,17 @@
-FetchContent_Declare(
-    trompeloeil
-    GIT_REPOSITORY https://github.com/rollbear/trompeloeil.git
-    GIT_TAG v42
+if(NOT TARGET trompeloeil::trompeloeil)
+    FetchContent_Declare(
+        trompeloeil
+        GIT_REPOSITORY https://github.com/rollbear/trompeloeil.git
+        GIT_TAG v42
     )
-FetchContent_Populate(trompeloeil)
+    FetchContent_Populate(trompeloeil)
 
-add_library(trompeloeil INTERFACE)
-target_include_directories(trompeloeil INTERFACE
-    $<BUILD_INTERFACE:${trompeloeil_SOURCE_DIR}/include/>
-    $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/Serenity/trompeloeil>
+    add_library(trompeloeil INTERFACE)
+    target_include_directories(trompeloeil INTERFACE
+        $<BUILD_INTERFACE:${trompeloeil_SOURCE_DIR}/include/>
+        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/trompeloeil>
     )
-add_library(Serenity::trompeloeil ALIAS trompeloeil)
+    add_library(trompeloeil::trompeloeil ALIAS trompeloeil)
 
-install(DIRECTORY ${trompeloeil_SOURCE_DIR}/include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/Serenity/trompeloeil)
+    install(DIRECTORY ${trompeloeil_SOURCE_DIR}/include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/trompeloeil)
+endif()
