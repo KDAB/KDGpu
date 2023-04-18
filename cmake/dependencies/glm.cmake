91a626d658336330cf817eaac994e5faa591ecc2
Paul Lemire <paul.lemire@kdab.com>
2023-04-18 16:54:40 +0200

    glm.cmake: Align with the one in Serenity and write cmake package file
    Change-Id: If65db5ef477bac481b2c7d98e73bd917a632ffb5
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/124762
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- cmake/dependencies/glm.cmake
+++ cmake/dependencies/glm.cmake
@@ -4,9 +4,25 @@ if(NOT TARGET glm::glm)
         GIT_REPOSITORY https://github.com/g-truc/glm.git
         GIT_TAG master
     )
-    FetchContent_Populate(glm)
 
-    add_library(glm INTERFACE)
+    if(NOT glm_POPULATED)
+        FetchContent_Populate(glm)
+
+        # Note: internally glm does
+        # add_library(glm INTERFACE)
+        # add_library(glm::glm ALIAS glm)
+        add_subdirectory(${glm_SOURCE_DIR} ${glm_BINARY_DIR})
+    endif()
+
+    # Work-around for:
+    # CMake Error in android/build/_deps/glm-src/glm/CMakeLists.txt:
+    # Target "glm" INTERFACE_INCLUDE_DIRECTORIES property contains path:
+    #
+    # "/home/kfunk/devel/src/kdab/serenity/android/build/_deps/glm-src/glm/../"
+    set_target_properties(glm PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ""
+    )
+
     target_include_directories(glm INTERFACE
         $<BUILD_INTERFACE:${glm_SOURCE_DIR}>
         $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/glm>
@@ -17,7 +33,23 @@ if(NOT TARGET glm::glm)
         GLM_ENABLE_EXPERIMENTAL
         GLM_LANG_STL11_FORCED
     )
-    add_library(glm::glm ALIAS glm)
 
     install(DIRECTORY ${glm_SOURCE_DIR}/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/glm)
+
+    # Create CMake Package File for glm so that it can be found with find_package(glm)
+    # and exports glm with namespace glm:: to link against glm::glm
+    install(TARGETS glm
+        EXPORT glmConfig
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    )
+    export(TARGETS glm
+        FILE "${CMAKE_CURRENT_BINARY_DIR}/glmConfig.cmake")
+
+    install(EXPORT glmConfig
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/glm
+        EXPORT_LINK_INTERFACE_LIBRARIES
+        NAMESPACE glm::
+    )
 endif()
