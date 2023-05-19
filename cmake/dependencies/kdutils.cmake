9308048bc36a73d4e2f0deec198f3649cadfb77c
Mike Krus <mike.krus@kdab.com>
2023-05-19 11:25:49 +0100

    Make developing both KDUtils and KDGpu together easier
    Developer can clone KDUtils in src/dependencies/kdutils and both will be
    built together so KDUtils doesn't need to be built and installed first.
    Will also be installed together.
    
    Change-Id: I49fbc614461fd7ded2f5d529572f18c3640ef36c
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/125850
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    Tested-by: Continuous Integration <build@kdab.com>

--- cmake/dependencies/kdutils.cmake
+++ cmake/dependencies/kdutils.cmake
@@ -7,20 +7,24 @@
 # Contact KDAB at <info@kdab.com> for commercial licensing options.
 #
 # KDUtils library
-find_package(KDUtils CONFIG)
+if(EXISTS "${CMAKE_SOURCE_DIR}/src/dependencies/kdutils")
+    add_subdirectory(${CMAKE_SOURCE_DIR}/src/dependencies/kdutils/)
+else()
+    find_package(KDUtils CONFIG)
 
-if(NOT KDUtils_FOUND)
-    FetchContent_Declare(
-        KDUtils
-        GIT_REPOSITORY ssh://codereview.kdab.com:29418/kdab/kdutils
-        GIT_TAG master
-        USES_TERMINAL_DOWNLOAD YES USES_TERMINAL_UPDATE YES
-    )
+    if(NOT KDUtils_FOUND)
+        FetchContent_Declare(
+            KDUtils
+            GIT_REPOSITORY ssh://codereview.kdab.com:29418/kdab/kdutils
+            GIT_TAG master
+            USES_TERMINAL_DOWNLOAD YES USES_TERMINAL_UPDATE YES
+        )
 
-    option(KDUTILS_BUILD_TESTS "Build the tests" OFF)
+        option(KDUTILS_BUILD_TESTS "Build the tests" OFF)
 
-    FetchContent_MakeAvailable(KDUtils)
+        FetchContent_MakeAvailable(KDUtils)
+    endif()
+
+    find_package(KDFoundation CONFIG)
+    find_package(KDGui CONFIG)
 endif()
-
-find_package(KDFoundation CONFIG)
-find_package(KDGui CONFIG)
