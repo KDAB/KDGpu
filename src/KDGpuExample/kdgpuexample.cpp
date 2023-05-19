d18d5643cef58dfe07edde7846c30a4bbb5e721a
Mike Krus <mike.krus@kdab.com>
2023-05-19 14:59:04 +0100

    Look for assets in share folder
    By default, look in the ${INSTALL_DIR}/share/assets if it exists
    before using the compile time since this might not be defined (if
    not using presets), and might be different once app is installed.
    
    TODO: if apps are built as bundles on macOS, this will be have to
    be different.
    Change-Id: I0e4c55da44ea8109cb0af6d833ff8b520a8a9dac
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/125851
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- src/KDGpuExample/kdgpuexample.cpp
+++ src/KDGpuExample/kdgpuexample.cpp
@@ -9,6 +9,7 @@
 */
 
 #include "kdgpuexample.h"
+#include <KDUtils/dir.h>
 
 #include <cstdlib>
 
@@ -19,6 +20,10 @@ std::string assetPath()
     const char *path = std::getenv("KDGPUEXAMPLE_ASSET_PATH");
     if (path)
         return path;
+
+    auto dir = KDUtils::Dir{ KDUtils::Dir::applicationDir().path() + "../share/assets" };
+    if (dir.exists())
+        return dir.path();
 
 #if defined(KDGPUEXAMPLE_ASSET_PATH)
     return KDGPUEXAMPLE_ASSET_PATH;
