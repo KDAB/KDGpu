9c7519561faed60b0309e6ffa2ff3b23d82e1f3a
Daniel Blaensdorf <daniel.blaensdorf@kdab.com>
2023-07-06 10:10:24 +0200

    Added two test for shader module and gpu semaphore
    Change-Id: I05eb7362e9f4e58dc495da3cc8edc93d9f4d30ae

---
+++ tests/gpu_semaphore/tst_gpu_semaphore.cpp
@@ -0,0 +1,95 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#include <KDGpu/gpu_semaphore.h>
+#include <KDGpu/device.h>
+#include <KDGpu/instance.h>
+#include <KDGpu/vulkan/vulkan_graphics_api.h>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+using namespace KDGpu;
+
+TEST_SUITE("GPU_Semaphore")
+{
+    std::unique_ptr<GraphicsApi> api = std::make_unique<VulkanGraphicsApi>();
+    Instance instance = api->createInstance(InstanceOptions{
+            .applicationName = "GPU_Semaphore",
+            .applicationVersion = SERENITY_MAKE_API_VERSION(0, 1, 0, 0) });
+    Adapter *discreteGPUAdapter = instance.selectAdapter(AdapterDeviceType::Default);
+    Device device = discreteGPUAdapter->createDevice();
+
+    TEST_CASE("Construction")
+    {
+        SUBCASE("A default constructed GpuSemaphore is invalid")
+        {
+            // GIVEN
+            GpuSemaphore s;
+            // THEN
+            REQUIRE(!s.isValid());
+        }
+
+        SUBCASE("A constructed GpuSemaphore from a Vulkan API")
+        {
+            // GIVEN
+            const GpuSemaphoreOptions gpuSemaphoreOptions{};
+
+            // WHEN
+            GpuSemaphore s = device.createGpuSemaphore(gpuSemaphoreOptions);
+
+            // THEN
+            CHECK(s.isValid());
+        }
+    }
+
+    TEST_CASE("Destruction")
+    {
+        // GIVEN
+        const GpuSemaphoreOptions gpuSemaphoreOptions{};
+
+        Handle<GpuSemaphore_t> gpuSemaphoreHandle;
+
+        SUBCASE("Going Out Of Scope")
+        {
+            {
+                // WHEN
+                GpuSemaphore s = device.createGpuSemaphore(gpuSemaphoreOptions);
+                gpuSemaphoreHandle = s.handle();
+
+                // THEN
+                CHECK(s.isValid());
+                CHECK(gpuSemaphoreHandle.isValid());
+                CHECK(api->resourceManager()->getGpuSemaphore(gpuSemaphoreHandle) != nullptr);
+            }
+
+            // THEN
+            CHECK(api->resourceManager()->getGpuSemaphore(gpuSemaphoreHandle) == nullptr);
+        }
+
+        SUBCASE("Move assignment")
+        {
+            // WHEN
+            GpuSemaphore s = device.createGpuSemaphore(gpuSemaphoreOptions);
+            gpuSemaphoreHandle = s.handle();
+
+            // THEN
+            CHECK(s.isValid());
+            CHECK(gpuSemaphoreHandle.isValid());
+            CHECK(api->resourceManager()->getGpuSemaphore(gpuSemaphoreHandle) != nullptr);
+
+            // WHEN
+            s = {};
+
+            // THEN
+            CHECK(api->resourceManager()->getGpuSemaphore(gpuSemaphoreHandle) == nullptr);
+        }
+    }
+}
---
+++ tests/shader_module/tst_shader_module.cpp
@@ -0,0 +1,40 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#include <KDGpu/shader_module.h>
+#include <KDGpu/device.h>
+#include <KDGpu/instance.h>
+#include <KDGpu/vulkan/vulkan_graphics_api.h>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+using namespace KDGpu;
+
+TEST_SUITE("Shader_Module")
+{
+    std::unique_ptr<GraphicsApi> api = std::make_unique<VulkanGraphicsApi>();
+    Instance instance = api->createInstance(InstanceOptions{
+            .applicationName = "Shader_Module",
+            .applicationVersion = SERENITY_MAKE_API_VERSION(0, 1, 0, 0) });
+    Adapter *discreteGPUAdapter = instance.selectAdapter(AdapterDeviceType::Default);
+    Device device = discreteGPUAdapter->createDevice();
+
+    TEST_CASE("Construction")
+    {
+        SUBCASE("A default constructed ShaderModule is invalid")
+        {
+            // GIVEN
+            ShaderModule s;
+            // THEN
+            REQUIRE(!s.isValid());
+        }
+    }
+}
