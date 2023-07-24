1dee31ea72fd6176bc2f0c1dde3b782c66fe3f0c
Daniel Blaensdorf <daniel.blaensdorf@kdab.com>
2023-07-24 09:50:20 +0200

    Simplification since dxc is part of the vulkan sdk
    Change-Id: I32bb66c6130bf2df275d1b6fdfaf8b69bf6f9d6f
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/128577
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- cmake/dependencies/dxc.cmake
+++ cmake/dependencies/dxc.cmake
@@ -6,36 +6,13 @@
 #
 # Contact KDAB at <info@kdab.com> for commercial licensing options.
 #
-
-cmake_policy(SET CMP0135 NEW)
-
 message("-- Loading DXC compiler...")
 
-if(NOT TARGET dxc::dxc)
-    if(WIN32)
-        set(DXC_FILE
-            https://ci.appveyor.com/api/projects/dnovillo/directxshadercompiler/artifacts/build%2FRelease%2Fdxc-artifacts.zip?branch=main&pr=false&job=image%3A%20Visual%20Studio%202022
-        )
-    elseif(NOT APPLE)
-        set(DXC_FILE
-            https://ci.appveyor.com/api/projects/dnovillo/directxshadercompiler/artifacts/build%2Fdxc-artifacts.tar.gz?branch=main&pr=false&job=image%3A%20Ubuntu
-        )
-    endif()
-
-    if(DXC_FILE)
-        FetchContent_Declare(dxc URL ${DXC_FILE})
-        FetchContent_MakeAvailable(dxc)
-
-        if(WIN32)
-            set(DXC_EXECUTABLE
-                ${dxc_SOURCE_DIR}/bin/dxc.exe
-                CACHE INTERNAL ""
-            )
-        elseif(NOT APPLE)
-            set(DXC_EXECUTABLE
-                ${dxc_SOURCE_DIR}/bin/dxc
-                CACHE INTERNAL ""
-            )
-        endif()
-    endif()
+find_program(DXC_EXECUTABLE dxc)
+if(NOT DXC_EXECUTABLE)
+    message(
+        FATAL_ERROR
+            "\ndxc (DirectXShaderCompiler) could not be found. "
+            "This tool is required to compile hlsl shaders. Consider adding the VulkanSDK/bin folder to your PATH.\n"
+    )
 endif()
