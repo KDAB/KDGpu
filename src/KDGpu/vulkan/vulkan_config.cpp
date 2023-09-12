04ba5ad2214c62423d79222641a7508eb5fd3392
BogDan Vatra <bogdan@kdab.com>
2023-09-12 15:54:43 +0300

    Fix static compilation
    Change-Id: I08b0dc147d8dc736c2bdba2fa45d40752fdfcadc
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/130603
    Reviewed-by: Mike Krus <mike.krus@kdab.com>

---
+++ src/KDGpu/vulkan/vulkan_config.cpp
@@ -0,0 +1,51 @@
+#include "vulkan_config.h"
+
+namespace KDGpu {
+
+std::vector<const char *> getDefaultRequestedInstanceExtensions()
+{
+    std::vector<const char *> extensions;
+    extensions.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
+#if defined(KDGPU_PLATFORM_LINUX)
+    extensions.push_back("VK_KHR_xcb_surface");
+    extensions.push_back("VK_KHR_wayland_surface");
+#elif defined(KDGPU_PLATFORM_WIN32)
+    extensions.push_back("VK_KHR_win32_surface");
+#elif defined(KDGPU_PLATFORM_MACOS)
+    extensions.push_back("VK_EXT_metal_surface");
+    extensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
+    extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+#endif
+    if (enableValidationLayers)
+        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    return extensions;
+}
+
+//
+// Device Config
+//
+std::vector<const char *> getDefaultRequestedDeviceExtensions()
+{
+    std::vector<const char *> extensions;
+    extensions.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
+// clang-format off
+#if defined(KDGPU_CUDA)
+    #if defined(KDGPU_PLATFORM_LINUX)
+        extensions.push_back(VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME);
+        extensions.push_back(VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME);
+    #elif defined(KDGPU_PLATFORM_WIN32)
+        extensions.push_back(VK_KHR_EXTERNAL_MEMORY_WIN32_EXTENSION_NAME);
+        extensions.push_back(VK_KHR_EXTERNAL_SEMAPHORE_WIN32_EXTENSION_NAME);
+    #endif
+#endif
+// clang-format on
+#if defined(VK_KHR_synchronization2)
+    extensions.push_back(VK_KHR_SYNCHRONIZATION_2_EXTENSION_NAME);
+#endif
+#if defined(KDGPU_PLATFORM_MACOS)
+    extensions.push_back("VK_KHR_portability_subset");
+#endif
+    return extensions;
+}
+
+} // namespace KDGpu
