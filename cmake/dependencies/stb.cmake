08f14fd3d30488ba89cbea3818e00748725275d0
Joshua Goins <joshua.goins@kdab.com>
2023-09-22 03:24:30 -0400

    Allow using system stb if found
    stb doesn't export a CMake config or anything else useful,
    unfortunately. However, since it's all headers it's incredibly easy to
    find via find_path and that's now checked before downloading with
    FetchContent.
    
    Change-Id: Ie2d60fdb5c35a9206cf3d4512f7b5cec48ba4809
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/131102
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- cmake/dependencies/stb.cmake
+++ cmake/dependencies/stb.cmake
@@ -7,8 +7,21 @@
 # Contact KDAB at <info@kdab.com> for commercial licensing options.
 #
 
-find_package(stb QUIET)
-if(NOT TARGET stb::stb)
+find_path(
+    STB_INCLUDE_DIR
+    NAMES stb_image.h
+    PATH_SUFFIXES stb
+)
+
+find_package_handle_standard_args(stb DEFAULT_MSG STB_INCLUDE_DIR)
+
+if(STB_FOUND)
+    mark_as_advanced(STB_INCLUDE_DIR)
+
+    add_library(stb INTERFACE)
+    target_include_directories(stb SYSTEM INTERFACE ${STB_INCLUDE_DIR})
+    add_library(stb::stb ALIAS stb)
+else()
     FetchContent_Declare(
         stb
         GIT_REPOSITORY https://github.com/nothings/stb.git
