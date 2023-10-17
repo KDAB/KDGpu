4474d8bc2444486d25fd28f40f31e18bf7608c58
Paul Lemire <paul.lemire@kdab.com>
2023-10-17 10:01:20 +0200

    KDGpuKDGui: use KDGUI_PLATFORM rather than KD_PLATFORM
    Change-Id: Ia2dac70a6129409e27d239b5496fb0e31d20e65a
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/132333
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    Tested-by: Continuous Integration <build@kdab.com>

--- src/KDGpuKDGui/view.cpp
+++ src/KDGpuKDGui/view.cpp
@@ -16,17 +16,17 @@
 #include <KDFoundation/core_application.h>
 #include <KDGui/config.h>
 
-#if defined(KD_PLATFORM_WIN32)
+#if defined(KDGUI_PLATFORM_WIN32)
 #include <KDGui/platform/win32/win32_platform_window.h>
 #endif
-#if defined(KD_PLATFORM_LINUX)
+#if defined(KDGUI_PLATFORM_XCB)
 #include <KDGui/platform/linux/xcb/linux_xcb_platform_window.h>
 #endif
 #if defined(KDGUI_PLATFORM_WAYLAND)
 #include <KDGui/platform/linux/wayland/linux_wayland_platform_window.h>
 #include <KDGui/platform/linux/wayland/linux_wayland_platform_integration.h>
 #endif
-#if defined(KD_PLATFORM_MACOS)
+#if defined(KDGUI_PLATFORM_COCOA)
 extern CAMetalLayer *createMetalLayer(KDGui::Window *window);
 #endif
 
@@ -53,14 +53,14 @@ View::~View()
 
 KDGpu::SurfaceOptions View::surfaceOptions(KDGui::Window *w)
 {
-#if defined(KD_PLATFORM_WIN32)
+#if defined(KDGUI_PLATFORM_WIN32)
     auto win32Window = static_cast<KDGui::Win32PlatformWindow *>(w->platformWindow());
     return KDGpu::SurfaceOptions{
         .hWnd = win32Window->handle()
     };
 #endif
 
-#if defined(KD_PLATFORM_LINUX)
+#if defined(KDGUI_PLATFORM_XCB)
     auto xcbWindow = static_cast<KDGui::LinuxXcbPlatformWindow *>(w->platformWindow());
     if (xcbWindow != nullptr) {
         return KDGpu::SurfaceOptions{
@@ -80,7 +80,7 @@ KDGpu::SurfaceOptions View::surfaceOptions(KDGui::Window *w)
     }
 #endif
 
-#if defined(KD_PLATFORM_MACOS)
+#if defined(KDGUI_PLATFORM_COCOA)
     return KDGpu::SurfaceOptions{
         .layer = createMetalLayer(w)
     };
