68a8463468a1539cff9a486fdf2d74e5c29f0f60
Alistair Baxter <alistair.baxter@kdab.com>
2024-01-30 13:15:40 +0000

    Hello Triangle example for Android
    Relies on kdgpu and kdgpuexample static libraries being built for
    Android armv8a first.
    
    Change-Id: Id42455c1df9ff1a413334b05917dd91782f58228

---
+++ examples/android/hello_triangle/hello_triangle.h
@@ -0,0 +1,45 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#pragma once
+
+#include <KDGpuExample/simple_example_engine_layer.h>
+
+#include <KDGpu/bind_group.h>
+#include <KDGpu/buffer.h>
+#include <KDGpu/graphics_pipeline.h>
+#include <KDGpu/render_pass_command_recorder_options.h>
+
+#include <glm/glm.hpp>
+
+using namespace KDGpuExample;
+
+class HelloTriangle : public SimpleExampleEngineLayer
+{
+public:
+protected:
+    void initializeScene() override;
+    void cleanupScene() override;
+    void updateScene() override;
+    void render() override;
+    void resize() override;
+
+private:
+    Buffer m_buffer;
+    Buffer m_indexBuffer;
+    PipelineLayout m_pipelineLayout;
+    GraphicsPipeline m_pipeline;
+    RenderPassCommandRecorderOptions m_opaquePassOptions;
+    CommandBuffer m_commandBuffer;
+
+    glm::mat4 m_transform;
+    Buffer m_transformBuffer;
+    BindGroup m_transformBindGroup;
+};
---
+++ examples/android/hello_triangle/main.cpp
@@ -0,0 +1,31 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#include "hello_triangle.h"
+
+#include <KDGpuExample/engine.h>
+
+#include <KDGui/gui_application.h>
+
+//![0]
+using namespace KDGui;
+using namespace KDGpu;
+using namespace KDGpuExample;
+
+int main()
+{
+    GuiApplication app;
+    app.applicationName = "Hello Triangle";
+    Engine engine;
+    auto exampleLayer = engine.createEngineLayer<HelloTriangle>();
+    engine.running = true;
+    return app.exec();
+}
+//![0]
