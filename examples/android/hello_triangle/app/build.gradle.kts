d06d3848e25ab3782278bdc375b7fe540a672a10
Alistair Baxter <alistair.baxter@kdab.com>
2024-01-30 13:15:40 +0000

    Hello Triangle example for Android
    Fetches KDGpu from git, and builds an Android app to contain the
    original hello triangle example. It is capable of being built for
    both 64-bit Arm and 64-bit x86 devices, such as the Android emulator.
    
    Change-Id: I20871efac9c861b9b036980820ee1fe6bf3402e3

---
+++ examples/android/hello_triangle/app/build.gradle.kts
@@ -0,0 +1,61 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.kdab.hellotriangle"
+    compileSdk = 34
+
+    defaultConfig {
+        ndk {
+            abiFilters += mutableSetOf("arm64-v8a", "x86_64")
+        }
+        applicationId = "com.kdab.hellotriangle"
+        minSdk = 32
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        prefab = true
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("../CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+    ndkVersion = "26.1.10909125"
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.games:games-activity:1.2.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
