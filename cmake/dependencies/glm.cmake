23176b7986ab4036dea30e99c6b07360f4cbdae6
Mike Krus <mike.krus@kdab.com>
2024-03-09 14:08:44 +0000

    Upgrade glm
    Fixes some C++20 related clang warnings
    
    Change-Id: I131dc679dd4bee786328cb3affae307f16e6e514
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/139097
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

--- cmake/dependencies/glm.cmake
+++ cmake/dependencies/glm.cmake
@@ -17,7 +17,7 @@ if(NOT TARGET glm::glm)
     FetchContent_Declare(
         glm
         GIT_REPOSITORY https://github.com/g-truc/glm.git
-        GIT_TAG 0.9.9.8
+        GIT_TAG 1.0.1
     )
 
     if(NOT glm_POPULATED)
@@ -43,25 +43,19 @@ if(NOT TARGET glm::glm)
         glm INTERFACE GLM_FORCE_RADIANS GLM_FORCE_DEPTH_ZERO_TO_ONE GLM_ENABLE_EXPERIMENTAL GLM_LANG_STL11_FORCED
     )
 
-    if (APPLE)
-        target_compile_options(
-            glm INTERFACE -Wno-deprecated-volatile
-        )
-    endif()
-
     install(DIRECTORY ${glm_SOURCE_DIR}/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/glm)
 
     # Create CMake Package File for glm so that it can be found with find_package(glm)
     # and exports glm with namespace glm:: to link against glm::glm
     install(
-        TARGETS glm
+        TARGETS glm glm-header-only
         EXPORT glmConfig
         ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
     )
     export(
-        TARGETS glm
+        TARGETS glm glm-header-only
         NAMESPACE glm::
         FILE "${glm_BINARY_DIR}/glmConfig.cmake"
     )
