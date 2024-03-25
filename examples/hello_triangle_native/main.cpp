fcb067ffa5c9b734d4217e2b43cb5bc1c19b6ca6
Joshua Goins <joshua.goins@kdab.com>
2024-03-25 09:27:08 -0400

    hello_triangle_native: Use dynamic_cast instead of static_cast
    The downcasts from windowing backends were using static_casts (which
    would always succeed) instead of dynamic_cast. This results in wrong
    behavior like the X11 backend being used on Wayland.
    
    Change-Id: Ib39689a9b73e4ea846763c5cd95ed34915ea06b0
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/139797
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

--- examples/hello_triangle_native/main.cpp
+++ examples/hello_triangle_native/main.cpp
@@ -139,14 +139,14 @@ int main()
 
     //![2]
 #if defined(KDGUI_PLATFORM_WIN32)
-    auto win32Window = static_cast<Win32PlatformWindow *>(window.platformWindow());
+    auto win32Window = dynamic_cast<Win32PlatformWindow *>(window.platformWindow());
     if (win32Window != nullptr) {
         surfaceOptions.hWnd = win32Window->handle();
     }
 #endif
 
 #if defined(KDGUI_PLATFORM_XCB)
-    auto xcbWindow = static_cast<LinuxXcbPlatformWindow *>(window.platformWindow());
+    auto xcbWindow = dynamic_cast<LinuxXcbPlatformWindow *>(window.platformWindow());
     if (xcbWindow != nullptr) {
         surfaceOptions.connection = xcbWindow->connection();
         surfaceOptions.window = xcbWindow->handle();
@@ -154,7 +154,7 @@ int main()
 #endif
 
 #if defined(KDGUI_PLATFORM_WAYLAND)
-    auto waylandWindow = static_cast<LinuxWaylandPlatformWindow *>(window.platformWindow());
+    auto waylandWindow = dynamic_cast<LinuxWaylandPlatformWindow *>(window.platformWindow());
     if (waylandWindow != nullptr) {
         surfaceOptions.display = waylandWindow->display();
         surfaceOptions.surface = waylandWindow->surface();
