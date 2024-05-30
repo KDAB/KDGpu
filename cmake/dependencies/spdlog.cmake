95c0a309e2313ee17d3ca9667cdfb9963a12a7d9
Sean Harmer <sean.harmer@kdab.com>
2024-05-30 10:51:37 +0100

    Use the same spdlog version and external fmt setup as KDUtils
    This fixes issues with MSVC 2022 screaming warnings about deprecations
    in the fmt version bundled with spdlog.
    
    Change-Id: Ibc8546cb08c979d79e5076ea8d158ae26f398062
    Reviewed-on: https://codereview.kdab.com/c/kdab/toy-renderer/+/142247
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>
    Tested-by: Continuous Integration <build@kdab.com>

--- cmake/dependencies/spdlog.cmake
+++ cmake/dependencies/spdlog.cmake
@@ -11,9 +11,28 @@
 # spdlog needs to be installed. If already exists in the prefix,
 # we don't want to override it, so first we try to find it.
 # If we don't find it, then we fetch it and install it
-find_package(spdlog 1.11.0 QUIET)
+find_package(spdlog 1.14.1 QUIET)
 
 if(NOT TARGET spdlog::spdlog)
+    # We need to use external fmt because the one bundled with spldog 1.x throws
+    # warnings in newer Visual Studio MSVC compiler versions.
+    # See https://github.com/gabime/spdlog/issues/2912
+    # TODO(spdlog2): external fmt can possibly be removed once splog 2.x is used
+    # which bundles newer fmt version
+    find_package(fmt 10.2.1 QUIET)
+    if(NOT TARGET fmt)
+        FetchContent_Declare(
+            fmt
+            GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+            GIT_TAG e69e5f977d458f2650bb346dadf2ad30c5320281 # 10.2.1
+        )
+        FetchContent_MakeAvailable(fmt)
+    endif()
+    set(SPDLOG_FMT_EXTERNAL_HO ON)
+    # with this spdlog is included as a system library and won't e.g. trigger
+    # linter warnings
+    set(SPDLOG_SYSTEM_INCLUDES ON)
+
     get_property(tmp GLOBAL PROPERTY PACKAGES_NOT_FOUND)
     list(
         FILTER
@@ -27,7 +46,7 @@ if(NOT TARGET spdlog::spdlog)
     FetchContent_Declare(
         spdlog
         GIT_REPOSITORY https://github.com/gabime/spdlog.git
-        GIT_TAG v1.11.0
+        GIT_TAG 27cb4c76708608465c413f6d0e6b8d99a4d84302 # v1.14.1
     )
     set(SPDLOG_INSTALL
         ON
