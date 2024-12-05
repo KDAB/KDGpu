0f1c9237fc6c648fb7eeccea55c055ec5122c69f
Sean Harmer <sean.harmer@kdab.com>
2024-12-05 10:47:24 +0000

    Update gradle project
    Change-Id: I88d406a9c44dfe9d80a9b5d73bd6ed4241aab23e
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdgpu/+/149361
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Alistair Baxter <alistair.baxter@kdab.com>

--- examples/android/hello_triangle/build.gradle.kts
+++ examples/android/hello_triangle/build.gradle.kts
@@ -9,5 +9,5 @@
 */
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.7.3" apply false
 }
