2bcbdfc8f1c1a80258d2ef128dd41861cf13b5f7
Alistair Baxter <alistair.baxter@kdab.com>
2025-02-27 20:04:09 +0000

    Android support for textured_quad example
    Auto-generated Android project with turbine
    Changed texture to be loaded with KDUtils functions because of Android
    filesystem restrictions.
    
    Task-Id: KDG-87
    Change-Id: Id89643a720af4a3fa2e4a4912f54c4e4993dc00e
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdgpu/+/152093
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>

---
+++ examples/textured_quad/android/app/build.gradle.kts
@@ -0,0 +1,85 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.kdab.textured_quad"
+    compileSdk = 34
+
+    defaultConfig {
+        ndk {
+            abiFilters += mutableSetOf("arm64-v8a")
+        }
+        applicationId = "com.kdab.textured_quad"
+        minSdk = 32
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        prefab = true
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("../CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+    ndkVersion = "26.1.10909125"
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.games:games-activity:1.2.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+
+// Task to copy files from outer project to android shader compilation folder
+tasks.register<Copy>("copyShaders") {
+    from("../../../../assets/shaders/examples/textured_quad")
+    into("src/main/shaders/examples/textured_quad")
+}
+
+// Task to copy font files
+tasks.register<Copy>("copyFonts") {
+    from("../../../../assets/fonts")
+    into("src/main/assets/fonts")
+}
+
+// Task to copy texture files
+tasks.register<Copy>("copyTextures") {
+    from("../../../../assets/textures")
+    into("src/main/assets/textures")
+}
+
+// Ensure the copyShaders task runs before the build task
+tasks.named("preBuild") {
+    dependsOn("copyShaders", "copyFonts", "copyTextures")
+}
--- examples/textured_quad/textured_quad.cpp
+++ examples/textured_quad/textured_quad.cpp
@@ -19,6 +19,9 @@
 #include <KDGpu/graphics_pipeline_options.h>
 #include <KDGpu/texture_options.h>
 
+#include <KDUtils/file.h>
+#include <KDUtils/dir.h>
+
 #include <glm/gtx/transform.hpp>
 
 //![1]
@@ -62,7 +65,25 @@ ImageData loadImage(const std::string &path)
     if (texturePath.rfind("/", 0) == 0)
         texturePath = texturePath.substr(1);
 #endif
+
+#ifdef PLATFORM_ANDROID
+    // Android filesystem requires our file wrapper to load the data
+    KDUtils::File file(KDUtils::File::exists(path) ? path : KDUtils::Dir::applicationDir().absoluteFilePath(path));
+
+    if (!file.open(std::ios::in | std::ios::binary)) {
+        SPDLOG_LOGGER_CRITICAL(KDGpu::Logger::logger(), "Failed to open file {}", path);
+        throw std::runtime_error("Failed to open file");
+    }
+
+    const KDUtils::ByteArray fileContent = file.readAll();
+    std::vector<uint32_t> buffer(fileContent.size() / 4);
+
+    auto _data = stbi_load_from_memory(
+            fileContent.data(), fileContent.size(), &_width, &_height, &texChannels, STBI_rgb_alpha);
+#else
     auto _data = stbi_load(texturePath.c_str(), &_width, &_height, &texChannels, STBI_rgb_alpha);
+#endif
+
     if (_data == nullptr) {
         SPDLOG_WARN("Failed to load texture {} {}", path, stbi_failure_reason());
         return {};
