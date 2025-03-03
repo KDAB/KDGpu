216f2e55a74740a43c06b366b318812dd0ef24e7
Alistair Baxter <alistair.baxter@kdab.com>
2025-03-03 14:30:41 +0000

    Multiview Android project
    Generated from Turbine
    
    Task-Id: KDG-87
    Change-Id: Ic2012c7c14d7fbb5ba80895ad7d2d5f18b72951e
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdgpu/+/152174
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>
    Tested-by: Continuous Integration <build@kdab.com>

---
+++ examples/multiview/android/settings.gradle.kts
@@ -0,0 +1,26 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "multiview"
+include(":app")
