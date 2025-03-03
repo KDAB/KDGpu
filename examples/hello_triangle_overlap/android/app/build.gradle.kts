49e20652887e9193581379dc4ec6b3242c435e2c
Alistair Baxter <alistair.baxter@kdab.com>
2025-03-03 14:03:48 +0000

    Hello Triangle Overlapped Android project
    Generated from Turbine
    
    Task-Id: KDG-87
    Change-Id: I0bcd8ef0c08f0b41cd0015a51b8e0c05be2d86c8
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdgpu/+/152172
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

---
+++ examples/hello_triangle_overlap/android/app/build.gradle.kts
@@ -0,0 +1,85 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.kdab.hello_triangle_overlap"
+    compileSdk = 34
+
+    defaultConfig {
+        ndk {
+            abiFilters += mutableSetOf("arm64-v8a")
+        }
+        applicationId = "com.kdab.hello_triangle_overlap"
+        minSdk = 32
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        prefab = true
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("../CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+    ndkVersion = "26.1.10909125"
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.games:games-activity:1.2.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+
+// Task to copy files from outer project to android shader compilation folder
+tasks.register<Copy>("copyShaders") {
+    from("../../../../assets/shaders/examples/hello_triangle_overlap")
+    into("src/main/shaders/examples/hello_triangle_overlap")
+}
+
+// Task to copy font files
+tasks.register<Copy>("copyFonts") {
+    from("../../../../assets/fonts")
+    into("src/main/assets/fonts")
+}
+
+// Task to copy texture files
+tasks.register<Copy>("copyTextures") {
+    from("../../../../assets/textures")
+    into("src/main/assets/textures")
+}
+
+// Ensure the copyShaders task runs before the build task
+tasks.named("preBuild") {
+    dependsOn("copyShaders", "copyFonts", "copyTextures")
+}
