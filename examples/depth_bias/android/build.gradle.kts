423cf72e7c5ae0c4d83920bd0bb20b6ec7bf8e86
Alistair Baxter <alistair.baxter@kdab.com>
2025-03-03 11:56:54 +0000

    Depth Bias Android project
    Generated from Turbine
    (edited to copy shaders from correct source)
    
    Task-Id: KDG-87
    Change-Id: I62d9997ef28dbbacf4b6f3d3fad9c9cd633a0e99
    Reviewed-on: https://codereview.kdab.com/c/kdab/kdgpu/+/152156
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Paul Lemire <paul.lemire@kdab.com>

---
+++ examples/depth_bias/android/build.gradle.kts
@@ -0,0 +1,13 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+}
