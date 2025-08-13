2ddcc210092b08d92a601faedb2c57c6bebe325d
Paul Lemire <paul.lemire@kdab.com>
2025-08-13 14:25:50 +0200

    WIP: Add example to demonstrate dynamic_rendering with subpasses
    Change-Id: I26b4d4f071775627bbc6e5ecb1920c8814fe3cba

---
+++ examples/render_to_texture_subpass_dynamic_rendering/render_to_texture_subpass_dynamic_rendering.h
@@ -0,0 +1,70 @@
+/*
+  This file is part of KDGpu.
+
+  SPDX-FileCopyrightText: 2025 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+
+  Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#pragma once
+
+#include <KDGpuExample/simple_example_engine_layer.h>
+
+#include <KDGpu/bind_group.h>
+#include <KDGpu/buffer.h>
+#include <KDGpu/graphics_pipeline.h>
+#include <KDGpu/render_pass.h>
+#include <KDGpu/render_pass_command_recorder_options.h>
+
+#include <glm/glm.hpp>
+
+class RenderToTextureSubpassDynamicRendering : public KDGpuExample::SimpleExampleEngineLayer
+{
+public:
+protected:
+    void initializeScene() override;
+    void cleanupScene() override;
+    void updateScene() override;
+    void render() override;
+    void resize() override;
+
+private:
+    void initializeMainScene();
+    void initializePostProcess();
+    void createOffscreenTexture();
+    void updateColorBindGroup();
+    void drawControls(ImGuiContext *ctx);
+
+    KDGpu::Buffer m_buffer;
+    KDGpu::Buffer m_indexBuffer;
+    KDGpu::Buffer m_transformBuffer;
+    KDGpu::Buffer m_fullScreenQuad;
+
+    KDGpu::PipelineLayout m_pipelineLayout;
+    KDGpu::GraphicsPipeline m_pipeline;
+    KDGpu::PipelineLayout m_postProcessPipelineLayout;
+    KDGpu::GraphicsPipeline m_postProcessPipeline;
+
+    glm::mat4 m_transform;
+    KDGpu::BindGroup m_transformBindGroup;
+    KDGpu::BindGroup m_colorBindGroup;
+    KDGpu::BindGroupLayout m_colorBindGroupLayout;
+
+    const KDGpu::PushConstantRange m_filterPosPushConstantRange{
+        .offset = 0,
+        .size = sizeof(float),
+        .shaderStages = KDGpu::ShaderStageFlags(KDGpu::ShaderStageFlagBits::FragmentBit)
+    };
+    std::vector<uint8_t> m_filterPosData;
+    float m_filterPos{ 0.0f };
+
+    // Rendering resources
+    const KDGpu::Format m_colorFormat{ KDGpu::Format::R8G8B8A8_UNORM };
+    KDGpu::Texture m_colorOutput;
+    KDGpu::TextureView m_colorOutputView;
+    KDGpu::Sampler m_colorOutputSampler;
+    KDGpu::RenderPassCommandRecorderWithDynamicRenderingOptions m_dynamicRenderPassOptions;
+    KDGpu::CommandBuffer m_commandBuffer;
+};
