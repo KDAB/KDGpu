88bd0ef7fbc96cd2aeacc7203195d9aafd7c32f7
Paul Lemire <paul.lemire@kdab.com>
2025-10-22 09:23:58 +0200

    Use vcpkg to fetch dependencies
    - KDGpuUtils: required some optional dependencies but was always added to build
    - KDXr: combine KDGPU_OPENXR_SUPPORT and KDGPU_BUILD_KDXR into a single variable
    
    Change-Id: I24a758e995500faa7d713a5567127fafb0f73e9e

--- cmake/dependencies/stb.cmake
+++ cmake/dependencies/stb.cmake
@@ -7,20 +7,28 @@
 # Contact KDAB at <info@kdab.com> for commercial licensing options.
 #
 
-find_package(stb QUIET)
+find_package(Stb REQUIRED)
 
 if(NOT TARGET stb::stb)
-    FetchContent_Declare(
-        stb
-        GIT_REPOSITORY https://github.com/nothings/stb.git
-        GIT_TAG master
-    )
-    FetchContent_MakeAvailable(stb)
 
     add_library(stb INTERFACE)
     target_include_directories(
-        stb SYSTEM INTERFACE $<BUILD_INTERFACE:${stb_SOURCE_DIR}> $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/stb>
+        stb INTERFACE $<BUILD_INTERFACE:${Stb_INCLUDE_DIR}> $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/stb>
     )
-    install(DIRECTORY ${stb_SOURCE_DIR}/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/stb)
     add_library(stb::stb ALIAS stb)
+
+    # Install only STB headers (not the entire vcpkg include directory)
+    file(GLOB STB_HEADERS "${Stb_INCLUDE_DIR}/stb_*.h")
+    if(STB_HEADERS)
+        install(FILES ${STB_HEADERS} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/stb)
+    endif()
+
+    # Install the target so it's available when this project is consumed
+    install(TARGETS stb EXPORT StbTargets)
+    install(
+        EXPORT StbTargets
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/stb
+        NAMESPACE stb::
+    )
+
 endif()
